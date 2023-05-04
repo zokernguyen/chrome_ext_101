58187e70e82a3ce6b2f458a0bd7f5af8ce14b283
Zoker <nguyenthanhtin1994@gmail.com>
2023-05-04 10:39:30 +0700

    tutorial continue

--- JerryHacks_Tutorials/popup/popup.js
+++ JerryHacks_Tutorials/popup/popup.js
@@ -1,2 +1,20 @@
 //Elements
 const locationIdElement = document.getElementById("locationId");
+const startDateElement = document.getElementById("startDate");
+const endDateElement = document.getElementById("endDate");
+
+//button elements
+
+const startButton = document.getElementById("startButton");
+const stopButton = document.getElementById("stopButton");
+
+startButton.onclick = function () {
+    if (startDateElement.value) {
+        console.log("Start date: ", startDateElement.value);
+    } else {
+        console.log("Start date is invalid!");
+    }
+};
+stopButton.onclick = function () {
+    console.log("End date: ", endDateElement.value);
+};
